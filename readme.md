d32ec98f1c054eb5d882fb83babc931b0c838cf9
Max CASA <max.lemaf@gmail.com>
2019-05-23 21:08:56 -0300

    exemplo geoJSON

--- readme.md
+++ readme.md
@@ -4,7 +4,50 @@
 
 ```javascript
 
-    L.tileLayer('http://car.semas.pa.gov.br/site/mosaicos/rapid-eye/{z}/{x}/{y}', {
-      tms: true
-    }).addTo(map);
+    var data = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+              [
+                [
+                  -53.558349609375,
+                  -3.3050503438610943
+                ],
+                [
+                  -54.31091308593749,
+                  -3.853293194062035
+                ],
+                [
+                  -54.12414550781249,
+                  -4.5161902548267
+                ],
+                [
+                  -53.3001708984375,
+                  -4.833733396156413
+                ],
+                [
+                  -52.2784423828125,
+                  -4.154679440673942
+                ],
+                [
+                  -52.470703125,
+                  -3.332470101396139
+                ],
+                [
+                  -53.558349609375,
+                  -3.3050503438610943
+                ]
+              ]
+            ]
+          }
+        }
+      ]
+    };
+
+    L.geoJSON(data).addTo(map);
 ```
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,19 +13,58 @@
     // Caso tenha algum analisador de código
     var L = $window.L;
 
-    var map = L.map('map', {
-      minZoom: 6,
-      maxZoom: 15,
-      // dragging: false
-    }).setView([-3.46956, -52.82227], 6);
+    var map = L.map('map').setView([-3.46956, -52.82227], 6);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.tileLayer('http://car.semas.pa.gov.br/site/mosaicos/rapid-eye/{z}/{x}/{y}', {
-      tms: true
-    }).addTo(map);
+    var data = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+              [
+                [
+                  -53.558349609375,
+                  -3.3050503438610943
+                ],
+                [
+                  -54.31091308593749,
+                  -3.853293194062035
+                ],
+                [
+                  -54.12414550781249,
+                  -4.5161902548267
+                ],
+                [
+                  -53.3001708984375,
+                  -4.833733396156413
+                ],
+                [
+                  -52.2784423828125,
+                  -4.154679440673942
+                ],
+                [
+                  -52.470703125,
+                  -3.332470101396139
+                ],
+                [
+                  -53.558349609375,
+                  -3.3050503438610943
+                ]
+              ]
+            ]
+          }
+        }
+      ]
+    };
+
+    L.geoJSON(data).addTo(map);
 
   }
 
