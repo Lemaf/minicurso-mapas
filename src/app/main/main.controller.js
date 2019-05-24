0d667bf1fdde205d6fdc66976fe6317f5c2fbe71
Bruno Piovesan Melchiori Peruzza <brunoperuzza@gmail.com>
2019-05-24 18:21:38 -0300

    Criação de marcadores com imagem alterada.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -19,9 +19,35 @@
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    var iconRat = L.icon({
+      iconUrl: '../../assets/images/rat.png',
+      iconSize: [38, 95],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowUrl: '../../assets/images/ratShadow.png',
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94]
+    });
+
+    var iconIronman = L.icon({
+      iconUrl: '../../assets/images/ironman.png',
+      iconSize: [38, 95],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+      shadowSize: [68, 95],
+      shadowAnchor: [22, 94]
+    });
+
+    var markerRat = L.marker([52.5, -0.09]).bindPopup('<img src="../../assets/images/rat.png" style="width: 20px; heigth: 20px;">');
+    var markerIronman = L.marker([52.7, -0.09]).bindPopup('Sim, o homem de ferro morre. <img src="https://i.ytimg.com/vi/Ix-k-MKHXWo/hqdefault.jpg" style="width: 200px; heigth: 200px;">');
+
+    L.setOptions(markerRat, {icon: iconRat})
+    L.setOptions(markerIronman, {icon: iconIronman})
+
+    markerRat.addTo(map);
+    markerIronman.addTo(map);
+
+    // markerIronman.openPopup();
 
   }
 
