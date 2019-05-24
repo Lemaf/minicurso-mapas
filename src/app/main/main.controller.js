4b0b59e7234b38a260ec4ba27d57a694244e1717
Maxmilian Ferreira de Lemos <max.lemaf@gmail.com>
2019-05-24 20:23:17 -0300

    interface clean

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,14 +13,14 @@
     // Caso tenha algum analisador de código
     var L = $window.L;
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([-3.6567033, -64.0763379], 3);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    L.marker([-3.6567033, -64.0763379]).addTo(map)
+        .bindPopup('Eae Pessoal...')
         .openPopup();
 
   }
