67b135e51b784aee1e7d7c14b194048fe3921ca6
Maxmilian Ferreira de Lemos <max.lemaf@gmail.com>
2019-05-24 20:46:43 -0300

    exemplo tileLayerWms

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,20 +8,36 @@
   /** @ngInject */
   function MainController($timeout, $window) {
 
-    // var vm = this;
+    var vm = this;
+    vm.ativar = ativar;
+    vm.layer = null;
 
-    // Caso tenha algum analisador de código
     var L = $window.L;
 
-    var map = L.map('map').setView([-3.6567033, -64.0763379], 3);
+    vm.map = L.map('map').setView([-3.6567033, -64.0763379], 3);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png').addTo(vm.map);
 
-    L.marker([-3.6567033, -64.0763379]).addTo(map)
-        .bindPopup('Eae Pessoal...')
-        .openPopup();
+    vm.layer = L.tileLayer.wms("http://amazonas01.ti.lemaf.ufla.br:8080/geoserver/areas_restritas_am/wms?", {
+      layers: 'areas_restritas_am:terra_indigena',
+      format: 'image/png',
+      transparent: true,
+      attribution: "Weather data © 2012 IEM Nexrad"
+    }).addTo(vm.map);
+
+    function ativar() {
+
+      if(vm.map.hasLayer(vm.layer)) {
+
+        vm.map.removeLayer(vm.layer);
+
+      } else {
+
+        vm.map.addLayer(vm.layer);
+
+      }
+
+    }
 
   }
 
